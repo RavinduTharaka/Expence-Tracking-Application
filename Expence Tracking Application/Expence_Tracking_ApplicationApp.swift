f8b79ded5b5ee0e931cdbad4cd6a75e874e8a301
RavinduTharaka <ravi.tharaka96@gmail.com>
2023-09-22 14:46:25 +0530

    Initial Commit

---
+++ Expence Tracking Application/Expence_Tracking_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expence_Tracking_ApplicationApp.swift
+//  Expence Tracking Application
+//
+//  Created by Ravindu Tharaka Ranathunga on 2023-09-22.
+//
+
+import SwiftUI
+
+@main
+struct Expence_Tracking_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
