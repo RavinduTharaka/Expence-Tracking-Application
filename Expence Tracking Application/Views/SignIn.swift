2422e03cf117b79773b4444dd42e77bf2c3b1ccf
RavinduTharaka <ravi.tharaka96@gmail.com>
2023-09-30 04:02:26 +0530

    login authentication

--- Expence Tracking Application/Views/SignIn.swift
+++ Expence Tracking Application/Views/SignIn.swift
@@ -10,67 +10,73 @@ import SwiftUI
 struct SignIn: View {
     @StateObject var loginVM : SigninViewModel = SigninViewModel()
     
+    @State private var wrongUsername = 0
+    @State private var wrongPassword = 0
+    @State private var ShowingLoginScreen = false
+    
     var body: some View {
-        ZStack{
-            Color(.white)
-                .edgesIgnoringSafeArea(.all)
-            VStack{
-                Text("Sign In")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .padding(.bottom, 30)
-                Image("avatar")
-                    .resizable()
-                    .scaledToFit()
-                
-                Spacer()
+        NavigationView{
+            ZStack{
+                Color("PrimaryColor").ignoresSafeArea()
+                Circle().scale(1.7).foregroundColor(.white.opacity(0.15))
+                Circle().scale(1.35).foregroundColor(.white)
                 VStack{
-                    RoundedRectangle(cornerRadius: 10)
-                        .foregroundColor(.white)
-                        .frame(height: 50)
-                        .overlay {
-                            TextField("Email", text: $loginVM.email)
-                                .font(.title3)
-                                .padding(.leading, 10)
-                                .frame(maxWidth: .infinity)
-                                .background(Color.white)
-                                .cornerRadius(50.0)
-                                .shadow(color: Color.black.opacity(0.08),radius: 60, x:0.0, y:16)
-                                .padding(.vertical)
-                        }
-                }
-                VStack{
-                    RoundedRectangle(cornerRadius: 10)
-                        .foregroundColor(.white)
-                        .frame(height: 50)
-                        .overlay {
-                            TextField("Password", text: $loginVM.password)
-                                .font(.title3)
-                                .padding(.leading, 10)
-                                .frame(maxWidth: .infinity)
-                                .background(Color.white)
-                                .cornerRadius(50.0)
-                                .shadow(color: Color.black.opacity(0.08),radius: 60, x:0.0, y:16)
-                                .padding(.vertical)
-                        }
-                }
-                Spacer()
                 
-                Text("Sign In")
-                    .font(.title3)
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                    .background{Color("PrimaryColor")
-                            .cornerRadius(50)
+                    Text("Sign In")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding()
+                    //                Image("avatar")
+                    //                    .resizable()
+                    //                    .scaledToFit()
+                    TextField("Email", text: $loginVM.email)
+                        .padding()
+                        .frame(width:300, height:50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width:CGFloat(wrongUsername))
+                    
+                    SecureField("Password", text: $loginVM.password)
+                        .padding()
+                        .frame(width:300, height:50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width:CGFloat(wrongPassword))
+                    
+                    Button("Login"){
+//                        authenticateUser(username: $loginVM.email, password: $loginVM.password)
+                        
+                    }.foregroundColor(.white)
+                        .frame(width: 300, height: 50)
+                        .background{Color("PrimaryColor")}
+                        .cornerRadius(10)
+                    
+                    NavigationLink(destination: Text("You are logged in @\(loginVM.email)"), isActive: $ShowingLoginScreen){
+                        EmptyView()
                     }
-                
-                Spacer()
-            }.padding()
+                }
+            }
+            .navigationBarHidden(true)
         }
     }
+    func authenticateUser(username: String, password: String){
+        if username.lowercased()=="ravindu"{
+            wrongPassword = 0
+            if password.lowercased()=="abc123"{
+                wrongPassword = 0
+                ShowingLoginScreen = true
+            }
+            
+            else{
+                wrongPassword = 2
+            }
+        }else{
+            wrongUsername = 2
+        }
+    }
+    
 }
+
 
 struct SignIn_Previews: PreviewProvider {
     static var previews: some View {
